9d2b01512aa4dcbd980a33e19fa288ce56f3193e
John Ortiz <jaortiz92@hotmail.com>
2021-08-30 15:13:38 -0500

    Primer commit class proveedor motor

---
+++ src/model/ProveedorMotor.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ProveedorMotor {
+    String nombreProveedor;
+    String direccion;
+    String telefono;
+
+    public ProveedorMotor(String nombreProveedor, String direccion, String telefono) {
+        this.nombreProveedor = nombreProveedor;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+}
